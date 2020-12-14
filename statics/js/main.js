7d7c8b883033357d5313ede639b87c27b347987c
Almyra Ramadhina <45095709+almyramadhina@users.noreply.github.com>
2020-12-15 00:21:03 +0700

    output json

--- statics/js/main.js
+++ statics/js/main.js
@@ -1,17 +1,17 @@
 function sendForm() {
     var year = parseInt(document.getElementById("year").value);
-    var hp = parseFloat(document.getElementById("hp").value);
-    var cyl = parseFloat(document.getElementById("cyl").value);
+    var hp = parseInt(document.getElementById("hp").value);
+    var cyl = parseInt(document.getElementById("cyl").value);
 
     var data = '';
 
     data = {
-        "tahun": year,
-        "engine hp": hp,
-        "engine cylinder": cyl
+        "year": year,
+        "enginehp": hp,
+        "enginecylinder": cyl
     };
 
     console.log(data);
     var reqdata = JSON.stringify(data);
-    window.localStorage.setItem('data', reqdata);
+    
   }
